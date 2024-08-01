993b7a3053dad4c03bac1e4dee7d7304c558794c
Avin Joy Dcosta <115493294+Avin-7@users.noreply.github.com>
2024-08-01 10:48:48 +0530

    changes in anchor tag

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,10 +5,14 @@ function Footer() {
     <div>
       <footer className=" flex justify-center align-middle bg-black text-gray-500 font-poppins py-12 ">
         <div className=" flex gap-8">
-          <a href="https://github.com/Avin-7" className=" hover:text-gray-800">
+          <a
+            href="https://github.com/Avin-7"
+            target="_blank"
+            className=" hover:text-gray-800"
+          >
             <ion-icon name="logo-github"></ion-icon>
           </a>
-          <a href="#">
+          <a href="https://avin-dcosta.vercel.app/" target="_blank">
             <ion-icon name="globe-outline"></ion-icon>
           </a>
         </div>
