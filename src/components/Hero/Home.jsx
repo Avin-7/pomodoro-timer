38c5c7e89e23e02d29d5ce30ee6769f4b960ece6
Avin Joy Dcosta <115493294+Avin-7@users.noreply.github.com>
2024-09-09 09:35:36 +0530

    Changed pomodoro to Foczy

--- src/components/Hero/Home.jsx
+++ src/components/Hero/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
         <div className="max-md:px-0 px-32 text-center flex flex-col gap-10">
           <h1 className="max-md:text-5xl max-md:px-4 text-7xl">
             Enhance Your <span className=" text-teal-100"> Productivity </span>
-            with Pomodoro.
+            with Foczy.
           </h1>
           <p className="max-md:px-16 max-md:text-md px-20 text-gray-400 ">
             Harness the power of focused work sessions with our Pomodoro
