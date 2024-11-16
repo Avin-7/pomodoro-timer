bdd53b5f05638efb833ac46f63c5e7e3c5dbd940
Avin Joy Dcosta <avinjoy51@gmail.com>
2024-11-16 21:36:35 +0530

    minor changes

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,58 +1,60 @@
 /* eslint-disable no-useless-catch */
-import { Client, Account, Databases, ID } from "appwrite";
-import conf from "../conf/conf"
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
 
 export class AuthService {
-        client = new Client();
-        account;
-    
-        constructor() {
-            this.client.setEndpoint(conf.appWriteUrl)
-            this.client.setProject(conf.appWriteProjectId)
-            this.account = new Account(this.client)
-        }
-        
-        async createAccount({ email, password, uname }){
-            // eslint-disable-next-line no-useless-catch
-            try {
-                const userAccount = await this.account.create(ID.unique(), email, password, uname);
-                if (userAccount) {
-                    return this.login({email,password})
-                } else {
-                    console.log(userAccount);
-                    return userAccount;
-                }
-            }
-            catch(error) {
-                console.log( error);
-            }
-        }
-        async login({ email, password }) {
-            try {
-                return await this.account.createEmailPasswordSession(email, password);
-            } catch (error) {
-                throw error;
-            }
-        }
-    
-        async getCurrentUser() {
-            try {
-              return await this.account.get();
-            } catch (error) {
-                throw error;
-            }
-        }
-        async logout() {
-            try {
-                await this.account.deleteSessions();
-            } catch (error) {
-                throw error;
-            }
-        }
-    
-    
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client.setEndpoint(conf.appWriteUrl);
+    this.client.setProject(conf.appWriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, uname }) {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        uname
+      );
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        console.log(userAccount);
+        return userAccount;
+      }
+    } catch (error) {
+      console.log(error);
     }
-    
-    const authService = new AuthService();
-    
-    export default authService;
+  }
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      throw error;
+    }
+  }
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
