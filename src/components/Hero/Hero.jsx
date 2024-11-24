0bb1fbf1c255ddf33595ec7b6fd5ddf7289f5db1
Avin Joy Dcosta <avinjoy51@gmail.com>
2024-11-24 10:08:56 +0530

    edit css styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import userContext from "../../context/userContext";
 import service from "../../appwrite/config";
 import Home from "./Home";
-import About from "../About/About"
+import About from "../About/About";
 import Footer from "../Footer/Footer";
 import { Modal, ConfigProvider } from "antd";
 function Hero() {
@@ -152,7 +152,7 @@ function Hero() {
     <>
       <Home />
       <div
-        className=" bg-neutral-950 text-white font-poppins pb-96 pt-96 max-md:pb-14 "
+        className=" bg-neutral-950 text-white font-poppins pb-96 pt-96"
         id="timer"
       >
         <h1 className="underline underline-offset-8  decoration-teal-200 text-center text-4xl pt-6 text-neutral-100 ">
