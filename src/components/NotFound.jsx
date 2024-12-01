a6f17275c7c119afeadc0fe65eb07534e62e660e
Avin Joy Dcosta <avinjoy51@gmail.com>
2024-12-01 10:37:43 +0530

    add 404 error page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div>404</div>
-  )
+    <div className=" text-white text-xl flex flex-col justify-center items-center mt-60">
+      <h1 className="text-center text-6xl font-bold">404</h1>
+      <h1 className="text-center">Page not found</h1>
+      <Link to={"/"} className=" px-4 py-3 bg-white rounded-md text-black m-8">
+        Go to home
+      </Link>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
